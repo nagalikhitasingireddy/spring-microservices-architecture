4ccac44eab17c0ba378e76f4c378c47f37579934
Singireddy Nagalikhita <nagalikithasingireddy@gmail.com>
2025-07-23 09:36:28 +0530

    user service edited for docker

--- User-service_Project-1/src/main/java/com/example/user/model/User.java
+++ User-service_Project-1/src/main/java/com/example/user/model/User.java
@@ -1,6 +1,7 @@
 package com.example.user.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 @Entity
 @Table(name = "users")
